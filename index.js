ef8b696fa8f153811393aceff59dcc4ed34bf83f
rizky putri <74160733+rizkyputri99@users.noreply.github.com>
2023-01-13 12:50:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import LandingPage from "./component/layout/LandingPage";
+
+import Coba from "./coba";
+
+export default function Home() {
+  return (
+    <div>
+      <LandingPage>
+          <Coba/>
+      </LandingPage>
+    </div>
+  )
+}
